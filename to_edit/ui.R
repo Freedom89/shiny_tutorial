06403968be6641de630eb54b6096a482f2fda2a4
Freedom89 <yxlowx@gmail.com>
2017-08-08 14:05:46 +0800

    added more dump

---
+++ to_edit/ui.R
@@ -0,0 +1,34 @@
+library(shinydashboard)
+header <- dashboardHeader(title="Example")
+
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+  )
+)
+
+body <-   dashboardBody(
+  tabItems(
+    # First tab content
+    tabItem(tabName = "dashboard",
+            fluidRow(
+              box(plotOutput("plot1", height = 250)),
+              
+              box(
+                title = "Controls",
+                sliderInput("slider", "Number of observations:", 1, 100, 50)
+              )
+            )
+    ),
+    
+    # Second tab content
+    tabItem(tabName = "widgets",
+            h2("Widgets tab content")
+    )
+  )
+)
+  
+  
+
+dashboardPage(header, sidebar, body,skin="black")
